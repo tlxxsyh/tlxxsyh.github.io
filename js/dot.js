a044eec47adf38bf4bc8ab29566642c0ceb62140
言念君君子 <61855391+tlxxsyh@users.noreply.github.com>
2023-03-24 23:09:19 +0800

    像素点领养的动态展示

--- js/dot.js
+++ js/dot.js
@@ -8,6 +8,32 @@ $("#dot_1").hover(function () {
     $("#dot-name").text("言念的像素点");
     $("#dot-colorID").text("颜色编号：#00FFFF");
     $("#dot-owner").text("领养者：Syhang");
+    $("#pixel-name").text("#00FFFF");
+    $("#pixel-owner").text("认养人：言念");
+    $(".pixel").css("background-color","#00FFFF");
+    $("#pixel-name").css("color","#00FFFF");
+}, function () {
+    $("#dot-message").css("opacity", "0");
+});
+$("#dot_2").hover(function () {
+    $("#dot-name").text("Frankie的像素点");
+    $("#dot-colorID").text("颜色编号：#00FFFF");
+    $("#dot-owner").text("领养者：Frankie");
+    $("#pixel-name").text("#00FFFF");
+    $("#pixel-owner").text("认养人：Frankie");
+    $(".pixel").css("background-color","#00FFFF");
+    $("#pixel-name").css("color","#00FFFF");
+}, function () {
+    $("#dot-message").css("opacity", "0");
+});
+$("#dot_3").hover(function () {
+    $("#dot-name").text("小小飞");
+    $("#dot-colorID").text("颜色编号：#335BA6");
+    $("#dot-owner").text("领养者：洪航飞");
+    $("#pixel-name").text("#335BA6");
+    $("#pixel-owner").text("认养人：洪航飞");
+    $(".pixel").css("background-color","#335BA6");
+    $("#pixel-name").css("color","#335BA6");
 }, function () {
     $("#dot-message").css("opacity", "0");
 });

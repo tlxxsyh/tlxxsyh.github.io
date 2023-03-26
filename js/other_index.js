8e583e544f483af14e05923fbb6a977ba90c4de2
言念君君子 <61855391+tlxxsyh@users.noreply.github.com>
2023-03-27 00:01:30 +0800

    新增了挑选食物功能

--- js/other_index.js
+++ js/other_index.js
@@ -1,3 +1,9 @@
 function pixel() {
     window.location = "https://tlxxsyh.github.io/html/other/pixel-pet.html";
 }
+function hack() {
+    window.location = "https://cybermap.kaspersky.com/cn";
+}
+function food() {
+    window.location = "https://tlxxsyh.github.io/html/other/food.html";
+}

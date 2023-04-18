5a8087c20ef25615b513a6601d1fc5401296b552
言念君君子 <61855391+tlxxsyh@users.noreply.github.com>
2023-04-18 20:31:05 +0800

    自动识别

--- js/question.js
+++ js/question.js
@@ -1,15 +1,36 @@
+var str = "{\n";
+
 function create() {
-    var str;
-    str = "{\n";
+    //读题目类型
     var str_type = '"type": "' + $('#subject').val() + '",\n';
+    //读题目问题
     var str_question = '"question": "' + $('#question-text').val() + '",\n';
+    //选项的读写
     var str_option = '"option": [\n';
-    var option = $('#option-textarea').val().split(',');
-    for(var i = 0; i  <option.length-1;i++){
-        str_option = str_option + '\t"' + i + '",\n';
+    var option = $('#option-textarea').val().split('、');
+    for (var i = 0; i < option.length; i++) {
+        option[i] = option[i].replace(/\r|\n|- | /ig, "");
+        if(i!=option.length-1){
+            option[i] = option[i].slice(0, -1);
+        }
+        str_option = str_option + '\t"' + option[i] + '",\n';
     }
-    str_option = str_option + '\t"' + option[option.length-1] + '"\n],\n';
-    var str_answer = '"answer": "'+$('#answer-text').val() + '"\n},';
-    
-    $('#json-my').val(str + str_type + str_question + str_option +str_answer);
+    // for(var i = 0; i  <option.length-1;i++){
+    //     str_option = str_option + '\t"' + i + '",\n';
+    // }
+    console.log(option);
+    //为了把末尾逗号删除
+    str_option = str_option.slice(0,-2);
+    str_option = str_option + "\n},\n";
+    //答案读取
+    var str_answer = '"answer": "' + $('#answer-text').val() + '"\n},';
+    str = str + str_type + str_question + str_option + str_answer;
+    $('#json-my').val(str);
+}
+
+function copy() {
+    var textarea = $('#json-my');
+    textarea.select();
+    document.execCommand('Copy');
+    textarea.val("复制成功！");
 }

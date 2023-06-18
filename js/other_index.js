49b510191a18a79849f6038761c4c0499f92c843
言念君君子 <61855391+tlxxsyh@users.noreply.github.com>
2023-06-18 23:46:19 +0800

    important modify

---
+++ js/other_index.js
@@ -0,0 +1,18 @@
+function pixel() {
+    window.location = "https://tlxxsyh.github.io/html/other/pixel-pet.html";
+}
+function hack() {
+    window.location = "https://cybermap.kaspersky.com/cn";
+}
+function food() {
+    window.location = "https://tlxxsyh.github.io/html/other/food.html";
+}
+function album() {
+    window.location = "https://tlxxsyh.github.io/html/other/album.html";
+}
+function movie() {
+    window.location = "https://tlxxsyh.github.io/html/other/movie.html";
+}
+function game() {
+    window.location = "https://tlxxsyh.github.io/html/other/game.html";
+}
